586279d4a51eed121fed3d30b7cda66781192d36
Maguire McMahon <mimcmahon20@vt.edu>
2025-03-19 13:05:09 -0400

    FIX: moving file structure

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Navigation } from "@/components/ui/navigation";
+import { Navigation } from "@/components/navigation";
 
 export const metadata: Metadata = {
   title: "NatureCards",
--- frontend/src/components/navigation.tsx
+++ frontend/src/components/navigation.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { UsersRound, Compass, Image, Home, MessageCircleHeart } from "lucide-react";
+import { UserRound, UsersRound, Compass, BookImage, MessageCircleHeart } from "lucide-react";
 
 interface NavElementProps {
   href: string;
@@ -19,14 +19,14 @@ const NavElement = ({ href, icon, label }: NavElementProps) => {
     <Link
       href={href}
       className={cn(
-        "flex flex-col items-center justify-center p-2 text-muted-foreground transition-colors",
-        isActive && " text-green-600 font-bold"
+        "flex flex-col items-center font-medium justify-center p-0 text-muted-foreground transition-all duration-300",
+        isActive && " text-green-600"
       )}
     >
       <div className="w-[50px] h-[50px] flex items-center justify-center">
         {icon}
       </div>
-      <span className="text-xs mt-1">{label}</span>
+      <span className="text-xs mt-0">{label}</span>
     </Link>
   );
 };
@@ -34,11 +34,11 @@ const NavElement = ({ href, icon, label }: NavElementProps) => {
 export function Navigation() {
   return (
     <nav className="flex fixed bottom-0 w-full items-center justify-around bg-background border-t border-border py-2">
-      <NavElement href="/social" icon={<MessageCircleHeart size={28} />} label="Social" />
-      <NavElement href="/feed" icon={<Home size={28} />} label="Feed" />
+      <NavElement href="/social" icon={<UsersRound size={28} />} label="Social" />
+      <NavElement href="/feed" icon={<MessageCircleHeart size={28} />} label="Feed" />
       <NavElement href="/" icon={<Compass size={28} />} label="Adventure" />
-      <NavElement href="/gallery" icon={<Image size={28} />} label="Gallery" />
-      <NavElement href="/user" icon={<UsersRound size={28} />} label="User" />
+      <NavElement href="/gallery" icon={<BookImage size={28} />} label="Gallery" />
+      <NavElement href="/user" icon={<UserRound size={28} />} label="User" />
     </nav>
   );
 } 
