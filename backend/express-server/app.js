c5a28ddc0ea6f53119ed82b03a2d17191f213ae8
Guido Visioni <gjvisioni@gmail.com>
2025-03-23 19:40:29 -0400

    Untested route skeletons added

--- backend/express-server/app.js
+++ backend/express-server/app.js
@@ -1,41 +1,150 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+// Import modules
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+// Set the web server
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.get(
+  "/",
+  (req, res) => res.send("<h1>NatureCards: Server</h1>") // Home web page
+);
+// Connect to MongoDB database
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  "mongodb+srv://naturecardsdev:nature123@naturecards.chz2p.mongodb.net/?retryWrites=true&w=majority&appName=naturecards",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+mongoose.connection.once("open", function () {
+  console.log("Connection with MongoDB was successful");
+});
+// Create routes for database access
+const tableSchema = require("./model");
+const router = express.Router();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/db", router);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Route to create a card
+router.post("/card/create", async (req, res) => {
+  try {
+    const user = await tableSchema.findById(req.body.userId);
+    user.cards.push(req.body.card);
+    await user.save();
+    res.status(201).json(user.cards);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Route to add a friend
+router.post("/friend/add", async (req, res) => {
+  try {
+    const user = await tableSchema.findById(req.body.userId);
+    user.friends.push(req.body.friendId);
+    await user.save();
+    res.status(201).json(user.friends);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
+// Route to get the friends array
+router.get("/friends/:userId", async (req, res) => {
+  try {
+    const user = await tableSchema.findById(req.params.userId);
+    res.status(200).json(user.friends);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Route to update the entire card array
+router.post("/cards/update", async (req, res) => {
+  try {
+    const user = await tableSchema.findById(req.body.userId);
+    user.cards = req.body.cards;
+    await user.save();
+    res.status(200).json(user.cards);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Route to get the card array
+router.get("/cards/:userId", async (req, res) => {
+  try {
+    const user = await tableSchema.findById(req.params.userId);
+    res.status(200).json(user.cards);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Route to handle login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await tableSchema.findOne({ username: req.body.username });
+    if (user && user.password === req.body.password) {
+      res.status(200).json(user);
+    } else {
+      res.status(401).json({ error: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Route to get user information
+router.get("/user/:userId", async (req, res) => {
+  try {
+    const user = await tableSchema.findById(req.params.userId);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Route to handle signup
+router.post("/signup", async (req, res) => {
+  try {
+    const newUser = new tableSchema(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json(response);
+  }
+});
+
+// Route to create a trade
+router.post("/trade/create", async (req, res) => {
+  try {
+    const user = await tableSchema.findById(req.body.userId);
+    user.trading.requests.push(req.body.trade);
+    await user.save();
+    res.status(201).json(user.trading.requests);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Route to remove a trade
+router.post("/trade/remove", async (req, res) => {
+  try {
+    const user = await tableSchema.findById(req.body.userId);
+    user.trading.requests = user.trading.requests.filter(
+      (trade) => trade._id.toString() !== req.body.tradeId
+    );
+    await user.save();
+    res.status(200).json(user.trading.requests);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Export the app to be used in bin/www.js
 module.exports = app;
