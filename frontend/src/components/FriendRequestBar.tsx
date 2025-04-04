863b8d38171276256a4b73d2edfef530a7fe76cf
ACarr33 <aidancarraretto@gmail.com>
2025-04-04 10:57:06 -0400

    Add mobile-friendly friend request bar styling

--- frontend/src/components/FriendRequestBar.tsx
+++ frontend/src/components/FriendRequestBar.tsx
@@ -9,23 +9,25 @@ interface FriendRequestBarProps {
 
 export function FriendRequestBar({ friend_request }: FriendRequestBarProps) {
   return (
-    <Card className="flex items-center p-4">
-      <Avatar className="w-12 h-12 mr-4">
-        <AvatarImage src={friend_request.profile_image} alt={friend_request.username} />
-      </Avatar>
-      <CardContent className="flex-1">
-        <p className="font-medium">{friend_request.username}</p>
-      </CardContent>
-      {friend_request.sender_id == "12345" ? ( //TODO: replace "12345" default with check for logged in user ID
-        <div>
-          <Button variant="outline" size="sm" onClick={() => handleProfileClick(friend_request._id)}>
+    <Card className="flex flex-col sm:flex-row items-start sm:items-center p-4 gap-4">
+      <div className="flex items-center w-full">
+        <Avatar className="w-12 h-12 mr-4">
+          <AvatarImage src={friend_request.profile_image} alt={friend_request.username} />
+        </Avatar>
+        <CardContent className="flex-1 p-0">
+          <p className="font-medium">{friend_request.username}</p>
+        </CardContent>
+      </div>
+      {friend_request.sender_id == "12345" ? (
+        <div className="flex flex-row gap-2 w-full sm:w-auto">
+          <Button variant="outline" size="sm" className="flex-1 sm:flex-none" onClick={() => handleProfileClick(friend_request._id)}>
             View Profile
           </Button>
-          <Button variant="outline" size="sm" style={{ marginLeft: '12px' }} onClick={() => handleAcceptFriend(friend_request._id)}>
-            Accept Friend Request
+          <Button variant="outline" size="sm" className="flex-1 sm:flex-none" onClick={() => handleAcceptFriend(friend_request._id)}>
+            Accept
           </Button>
-          <Button variant="outline" size="sm" style={{ marginLeft: '12px' }} onClick={() => handleDeclineFriend(friend_request._id)}>
-            Decline Friend Request
+          <Button variant="outline" size="sm" className="flex-1 sm:flex-none" onClick={() => handleDeclineFriend(friend_request._id)}>
+            Decline
           </Button>
         </div>
       ) : (
